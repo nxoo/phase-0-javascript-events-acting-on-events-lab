15e4833479d175de6dd3c925a65d233b2382203a
nxoo <nxoo.dev@gmail.com>
2022-08-29 01:59:14 +0300

    test passed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#ff69b4";
+
+function moveDodgerLeft() {
+    let leftValue = dodger.style.left.replace("px", "");
+    let leftInt = parseInt(leftValue, 10);
+
+    if (leftInt > 0) {
+        dodger.style.left = `${leftInt - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const rightvalue = dodger.style.left.replace("px", "");
+    const rightInt = parseInt(rightvalue, 10);
+
+    if (rightInt < 360) {
+        dodger.style.left = `${rightInt + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 37) {
+        moveDodgerLeft();
+    }
+    if (event.keyCode === 39) {
+        moveDodgerRight();
+    }
+});
+
